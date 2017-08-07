ddcce449666dbe5a2cee0816b20910d2ac9e0ca6
rooh_gogoli <amiriruhollah@gmail.com>
2017-08-07 17:13:20 -0600

    added new file for seperating large and small scale fading

--- PA3.m
+++ PA3.m
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Power Allocation in a femtocell network based on dual decomposition
-% This file is written based on solution in page 1 of Aug 1st, 2017
+% This file is written based on solution in page 1 of Aug 7th, 2017
 % notes
 % In this optimization problem Pmin is not considered in the constraints.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function Q = PASe(fbsCount, NumRealization,MaxIteration, showPlot)
+function Q = PA3(fbsCount, NumRealization,MaxIteration, showPlot)
 format long;
 %% Initialization
 %clear;
